5f77947ada971fc5290afebf7e3cdc85fc89906f
Leonard <leonard.horne@gmail.com>
2017-08-10 14:10:26 -0400

    Clean up code. Add semicolons. Add edit. Add undo.

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -4,55 +4,84 @@ const models = require("../models");
 const Todos = models.todo;
 
 router.get("/", (req, res) => {
-  Todos.findAll().then(todos => {
+  Todos.findAll()
+  .then(todos => {
     res.render("todos/index", { todos: todos });
-  });
+  })
+  .catch(err => {
+    throw(err);
+  })
 });
 
 router.get("/:id", (req, res) => {
-  Todos.findById(req.params.id).then(todo => {
+  Todos.findById(req.params.id)
+  .then(todo => {
     res.render("todos/show", { todo: todo });
-  });
-});
+  })
+})
 
 router.post("/add", (req, res) => {
   Todos.create({
     description: req.body.addTodo,
     completed: false
   })
-  res.redirect("/todos")
+  res.redirect("/todos");
 })
 
 router.post("/:id/done", (req, res) => {
   Todos.findById(parseInt(req.params.id))
   .then(todo => {
-    todo.update({ completed: true})
+    todo.update({ completed: true});
   })
   .then(todo => {
     res.redirect("/todos");
   })
   .catch(err => {
-    throw (err)
+    throw (err);
+  })
+})
+
+router.post("/:id/undo", (req, res) => {
+  Todos.findById(parseInt(req.params.id))
+  .then(todo => {
+    todo.update({ completed: false});
+  })
+  .then(todo => {
+    res.redirect("/todos");
+  })
+  .catch(err => {
+    throw (err);
   })
 })
 
 router.post("/:id/delete", (req, res) => {
   Todos.findById(parseInt(req.params.id))
   .then(todo => {
-    todo.destroy()
+    todo.destroy();
   })
   .then(todo => {
     res.redirect("/todos");
   })
   .catch(err => {
-    throw (err)
+    throw (err);
+  })
+})
+
+router.get("/:id/edit", (req, res) => {
+  Todos.findById(parseInt(req.params.id))
+  .then(todo => {
+    res.render("todos/edit", { todo: todo });
   })
 })
 
 router.post("/:id/edit", (req, res) => {
+  console.log(req.body)
   Todos.findById(parseInt(req.params.id))
   .then(todo => {
-    res.redirect("/todos/:id/edit")
+    todo.update({ description: req.body.editTodo });
+  })
+  .catch(err => {
+    throw (err);
   })
   res.redirect("/todos");
 })
